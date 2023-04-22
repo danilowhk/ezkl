9114c70b2de332b67219958515fb6fab554fb174
danilowhk <danilowhk@gmail.com>
2023-04-22 19:25:17 -0300

    fix

--- src/execute.rs
+++ src/execute.rs
@@ -363,7 +363,7 @@ fn forward(
 
 fn mock(data: String, logrows: u32) -> Result<(), Box<dyn Error>> {
     info!("Before prepare data");
-    let data = prepare_d ata(data)?;
+    let data = prepare_data(data)?;
     info!("Before model");
     let model = Model::from_arg()?;
     info!("Before circuit");
